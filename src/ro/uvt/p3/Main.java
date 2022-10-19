d1d2c116032d0f7b089becc98b38c69b06570596
km10130533 <katalin.melnic02@e-uvt.ro>
2022-10-19 13:47:28 +0300

    k

--- src/ro/uvt/p3/Main.java
+++ src/ro/uvt/p3/Main.java
@@ -5,11 +5,11 @@ import ro.uvt.p3.io.OutputDevice;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         InputDevice id = new InputDevice();
         OutputDevice od = new OutputDevice();
         Application app = new Application(id, od);
-
         app.run();
     }
 }
